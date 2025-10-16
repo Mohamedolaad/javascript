157995977a1ffd3a6af7a0f57c40c88c53772149
Mohamedolaad <olaadmohaa@gmail.com>
2025-10-16 17:00:32 +0300

    lesson 1 and 2

---
+++ functions/script.js
@@ -0,0 +1,60 @@
+// function girls () {
+// console.log("Girls like kindness.");
+// console.log("Girls like honesty.");
+// console.log("Girls like confidence.");
+// console.log("Girls like good communication.");
+// console.log("Girls like respect.");
+// console.log("Girls like being listened to.");
+// console.log("Girls like fashion.");
+// console.log("Girls like music.");
+// console.log("Girls like chocolate.");
+// console.log("Girls like traveling.");
+// console.log("Girls like cute surprises.");
+// console.log("Girls like spending time with friends.");
+// console.log("Girls like laughter.");
+// console.log("Girls like self-care.");
+// console.log("Girls like supportive people.");
+// console.log("Girls like creativity.");
+// console.log("Girls like compliments.");
+// console.log("Girls like good vibes.");
+// console.log("Girls like fun adventures.");
+// console.log("Girls like being appreciated.");
+// }
+
+
+
+
+// function Boys () {
+// console.log("Boys like respect.");
+// console.log("Boys like honesty.");
+// console.log("Boys like confidence.");
+// console.log("Boys like loyalty.");
+// console.log("Boys like support.");
+// console.log("Boys like good communication.");
+// console.log("Boys like adventure.");
+// console.log("Boys like music.");
+// console.log("Boys like sports.");
+// console.log("Boys like cars.");
+// console.log("Boys like gaming.");
+// console.log("Boys like peace of mind.");
+// console.log("Boys like being trusted.");
+// console.log("Boys like funny people.");
+// console.log("Boys like good food.");
+// console.log("Boys like style.");
+// console.log("Boys like real friendships.");
+// console.log("Boys like motivation.");
+// console.log("Boys like success.");
+// console.log("Boys like being appreciated.");
+// }
+// Boys()
+// girls()
+
+function multiply(num1, num2) {return num1 * num2
+
+}
+
+console.log( multiply (3, 3))
+
+function add (m1, m2)
+ {return  m1 + m2 }
+console.log (add (4, 6))
---
+++ variable_datatypes/variable.js
@@ -0,0 +1,16 @@
+// var firstName = "Mohamed" //String
+// var lastName = "Olaad" // String
+// var age = 23  //Number
+// var isMaried =false // boolean
+
+// console.log (firstName)
+// console.log(lastName)
+// console.log(age * 2)
+// console.log(isMaried)
+// var myCity = null
+// var myCity = "Kisumu"
+// var myCity = "Mogadishu"
+// console.log(myCity)
+
+ var address = "12th strt, Nairobi"
+console.log( typeof address)
