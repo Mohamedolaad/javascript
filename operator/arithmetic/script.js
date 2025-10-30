3711cc74e63a306dd95b1ff6b6f43fce66dff2cc
Mohamedolaad <olaadmohaa@gmail.com>
2025-10-30 07:02:02 +0300

    added operators

---
+++ operator/arithmetic/script.js
@@ -0,0 +1,64 @@
+// +
+// -
+// *
+// /
+// %
+// ++
+// --
+
+// // addition
+// var boys = 7
+
+// var girls = 3
+
+// var total = boys + girls 
+
+// console.log("result is " + total)
+
+
+// // subtraction
+// var boys = 7
+
+// var girls = 3
+
+// var total = boys - girls 
+
+// console.log("result is " + total)
+
+// multiplication
+
+// var boys = 7
+
+// var girls = 3
+
+// var total = boys * girls 
+
+// console.log("result is " + total)
+
+// division
+
+
+// var boys = 7
+
+// var girls = 3
+
+// var total = boys / girls 
+
+// console.log("result is " + total)
+
+// modulus
+
+var boys = 7
+
+var girls = 3
+
+var total = boys % girls 
+
+console.log("result is " + total)
+
+boys++
+
+console.log("boys " + boys)
+
+girls--
+console.log("girls " + girls )
---
+++ operator/logical/script.js
@@ -0,0 +1,43 @@
+// let age = 20;
+// let hasID = true;
+
+// if (age >= 18 && hasID) {
+//   console.log("Allowed to enter");
+// } else {
+//   console.log("Not allowed");
+// }
+// let age = 15
+// let hasID =true
+// if(age >= 14 && hasID) {
+//     console.log("allow to enter")
+// }
+// else {
+//     console.log("not allow to enter")
+// }
+
+
+// let age = 15
+// let hasID =true
+// if(age < 10 && hasID) {
+//     console.log("allow to enter")
+// }
+// else {
+//     console.log("not allow to enter")
+
+// }
+
+// let hasPassword = false;
+// let isAdmin = true;
+
+// if (hasPassword || isAdmin) {
+//   console.log("Wi-Fi access granted");
+// } else {
+//   console.log("Access denied");
+let hasPassword = false;
+let isAdmin = true;
+
+if ( hasPassword && isAdmin) {
+  console.log("Wi-Fi access granted");
+} else {
+  console.log("Access denied");
+}
