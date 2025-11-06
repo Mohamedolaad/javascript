8690ed0d45b7e033c9f371de3445ea04fa947b52
Mohamedolaad <olaadmohaa@gmail.com>
2025-11-06 07:24:51 +0300

    i added sring

---
+++ string/string.js
@@ -0,0 +1,17 @@
+var firstName = "Mohamed Abdikadir Olaad"
+// let lastName = ""
+// let age"5"
+// let isStudent"true"
+// let numberOfBrother = 5
+// let hasPassport =true
+// console.log(firstName.indexOf("M"))
+// console.log(firstName.indexOf("o"))
+// console.log(firstName.indexOf("h"))
+// console.log(firstName.charAt(3))
+// console.log(firstName.charAt(4))
+// console.log(firstName[2])
+// console.log(firstName.length)
+// console.log(firstName.toUpperCase())
+// console.log(firstName.toLowerCase())
+// console.log(firstName.trim())
+console.log(firstName.split(" "))
